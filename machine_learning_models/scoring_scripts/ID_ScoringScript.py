9bec96cfc4bb14b60c7cd8763c69b3384a73cde1
Leo Martinez III <123003509+LeoMartinezTAMUK@users.noreply.github.com>
2024-05-08 11:39:38 -0500

    Add files via upload

---
+++ machine_learning_models/scoring_scripts/ID_ScoringScript.py
@@ -0,0 +1,20 @@
+# Intrusion Detection (ID) Scoring Script
+# This script is used for testing the trained machine learning model on new actual (possibly real-time) data
+
+import pickle
+import numpy as np
+
+# Load the saved model
+with open('intrusionDetection.pkl', 'rb') as f: # rb stands for read binary
+    loaded_model = pickle.load(f)
+
+# Assuming X_new is your new data for prediction (The Real Data is X_new)
+num_samples = 10  # Number of samples (adjust as needed)
+num_features = 38  # Number of features (adjust as needed)
+X_new = np.zeros((num_samples, num_features))  # Placeholder values are zeros 
+
+# Make predictions using the loaded model (The model is expecting a 2D array (such as an Excel Spreadsheet)
+predictions = loaded_model.predict(X_new) # Based on the model, it will generate new predictions for the data it is provided
+
+# The model will make predictions based on both its previous trainig plus the information from the new data
+print(predictions)
